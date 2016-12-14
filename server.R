55be7a7140efde358b5bdd7f26b1f04e40d373f4
Josh Keating <josh.keating7@gmail.com>
2016-12-13 21:52:33 -0800

    messing around

--- server.R
+++ server.R
@@ -27,15 +27,39 @@ shinyServer(function(input, output) {
     # sets NA values to 0
     year.trimmed[is.na(year.trimmed)] <- 0
     
+    # year.trimmed <- arrange(year.trimmed, desc(Average_GPA))
     
-    
+    year.trimmed <- aggregate(year.trimmed$Average_GPA, list(year.trimmed$Primary_Instructor), mean)
     
     # return(year.trimmed)
     
     
-    p <- plot_ly(year.trimmed, x = ~Primary_Instructor, y = ~Average_GPA, type = 'bar', name = 'Autum') %>%
-      # add_trace(y = ~LA_Zoo, name = 'LA Zoo') %>%
-      layout(yaxis = list(title = 'Average GPA'), barmode = 'group')
+    # ax <- list(
+    #   type = "Primary_Instructor",
+    #   categoryorder = "Primary_Instructor ascending",
+    #   showgrid = TRUE,
+    #   showline = TRUE,
+    #   autorange = TRUE,
+    #   showticklabels = TRUE,
+    #   ticks = "outside",
+    #   tickangle = 0
+    # )
+    
+    
+    
+    # p <- plot_ly(year.trimmed, x = ~Primary_Instructor, y = ~Average_GPA, type = 'bar', name = 'Average GPA') %>%
+    #   add_trace(y = ~c(Autumn, Winter), name = 'Autumn') %>%
+    #   
+    #   layout(yaxis = list(title = 'Average GPA'), barmode = 'group')
+    # return(p)
+    # 
+    
+    p <- plot_ly(year.trimmed, x = ~x, y = ~Group.1, type = 'bar', name = 'Average GPA') %>%
+      # add_trace(y = ~A, name = 'A') %>%
+      # # add_trace(y = ~'A-', name = 'A-') %>%
+      # add_trace(y = ~B, name = 'B') %>%
+      # # add_trace(y = ~'B-', name = 'B-') %>%
+      layout(yaxis = list(title = 'Average GPA'), barmode = 'stack')
       return(p)
     
   })
